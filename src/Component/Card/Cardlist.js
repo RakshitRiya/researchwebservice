4ca10f90308e00aa302f15d2043900b5d16b71ac
Riya Rakshit <RBR.RAKSHIT14@GMAIL.COM>
2020-04-23 11:56:54 +0000

    second commit

--- src/Component/Card/Cardlist.js
+++ src/Component/Card/Cardlist.js
@@ -4,7 +4,7 @@ import Card from './Card';
 const Cardlist = ({SearchResult}) => {
     return(
         <div>
-            <h2 className="tc f1-l fw1 white">Search Results</h2>
+            <h2 className="tc f1-l fw1">Search Results</h2>
             {
                 SearchResult.map((item, key) => {
                     return(
--- src/Component/Searchbar/Searchbar.js
+++ src/Component/Searchbar/Searchbar.js
@@ -4,8 +4,8 @@ import './Searchbar.css'
 const Searchbar = ({fetchResults ,searchChange}) => {
         return(
             <div className='container'>
-                <h1 className="f2 f1-l fw2 white-90 mb0 lh-title">Wikipedia</h1>
-                <h2 className="fw1 f3 white-80 mt3 mb4">Search Engine- Research about a Person</h2>
+                <h1 className="f2 f1-l fw2 mb0 lh-title">Wikipedia</h1>
+                <h2 className="fw1 f3  mt3 mb4">Search Engine- Research about a Person</h2>
                 <form onSubmit={fetchResults}  className="search-form ba b--white">
                     <input onChange={searchChange} required pattern=".*\S+.*" title="Please don't use white spaces :)"   type="text" className="search-field" placeholder="What are you looking for?" />
                     <button type="submit" className="button-search"><i className="fa fa-search fa-lg"></i></button>
